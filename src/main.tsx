43a23c98bbf31217a23b3965758dc76d1f8d367c
Huh... <rel270606@gmail.com>
2025-10-18 14:10:40 +1000

    some part of assets.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
+import {createContext, StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import AuthStore from "./store/AuthStore.ts";
+
+interface State{
+    store: {
+        AuthStore: AuthStore,
+    }
+}
+
+export const store = {
+    AuthStore: new AuthStore()
+}
+
+export const Context = createContext<State>({
+    store,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Context value={{store}}>
+          <App />
+      </Context>
   </StrictMode>,
 )
